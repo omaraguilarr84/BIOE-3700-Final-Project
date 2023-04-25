4ee12b58b30f1a155beef3db8c8fed07136ef981
Tdl260 <130683542+Tdl260@users.noreply.github.com>
2023-04-25 17:33:41 -0400

    Add files via upload

--- AaronThinksHeIsCool.ino
+++ AaronThinksHeIsCool.ino
@@ -1,11 +1,18 @@
-const int OUT_PIN = A0;
+const int OUT_PIN1 = A0;
+const int OUT_PIN2 = A1;
+const int OUT_PIN3 = A2;
 const int SAMPLE_TIME = 10;
 unsigned long millisCurrent;
 unsigned long millisLast = 0;
 unsigned long millisElapsed = 0;
 
-int sampleBufferValue = 0;
-int maxSampleBufferValue = 0;
+int sampleBufferValue1 = 0;
+int maxSampleBufferValue1 = 0;
+int sampleBufferValue2 =0;
+int maxSampleBufferValue2 = 0;
+int sampleBufferValue3 = 0;
+int maxSampleBufferValue3 = 0;
+
 unsigned long millisLast2 = 0;
 unsigned long millisElapsed2 = 0;
 
@@ -21,21 +28,43 @@ void loop() {
   millisElapsed2 = millisCurrent - millisLast2;
   
 
-  if (digitalRead(OUT_PIN) == LOW) {
-    sampleBufferValue++;
+  if (digitalRead(OUT_PIN1) == LOW) {
+    sampleBufferValue1++;
+  }
+  if (digitalRead(OUT_PIN2) == LOW) {
+    sampleBufferValue2++;
+  }
+  if (digitalRead(OUT_PIN3) == LOW) {
+    sampleBufferValue3++;
   }
 
   if (millisElapsed > SAMPLE_TIME) {
-    //Serial.println(sampleBufferValue);
-    if (sampleBufferValue > maxSampleBufferValue){
-          maxSampleBufferValue = sampleBufferValue;
-          Serial.println(maxSampleBufferValue);
+    
+    if (sampleBufferValue1 > maxSampleBufferValue1){
+          maxSampleBufferValue1 = sampleBufferValue1;
+          Serial.println("Mic 1: ");
+          Serial.println(maxSampleBufferValue1);
       }
-    sampleBufferValue = 0;
+    if (sampleBufferValue2 > maxSampleBufferValue2){
+          maxSampleBufferValue2 = sampleBufferValue2;
+          Serial.println("Mic 2: ");
+          Serial.println(maxSampleBufferValue2);
+      }
+    if (sampleBufferValue3 > maxSampleBufferValue3){
+          maxSampleBufferValue3 = sampleBufferValue3;
+          Serial.println("Mic 3: ");
+          Serial.println(maxSampleBufferValue3);
+      }
+
+    sampleBufferValue1 = 0;
+    sampleBufferValue2 = 0;
+    sampleBufferValue3 = 0;
     millisLast = millisCurrent;
   }
-  if (millisElapsed2 > 1500){
-        maxSampleBufferValue = 0; 
+  if (millisElapsed2 > 5000){
+        maxSampleBufferValue1 = 0; 
+        maxSampleBufferValue2 = 0; 
+        maxSampleBufferValue3 = 0;
         millisLast2 = millisCurrent;
   }
  
